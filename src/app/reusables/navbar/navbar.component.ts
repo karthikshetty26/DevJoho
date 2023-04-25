e7fcd0ae9959703af5f03ccfa8fa65f820cca7bc
Karthik Shetty <karthikkanyana26@gmail.com>
2023-04-26 00:36:11 +0530

    Updated nav bar functionality

--- src/app/reusables/navbar/navbar.component.ts
+++ src/app/reusables/navbar/navbar.component.ts
@@ -12,22 +12,32 @@ export class NavbarComponent {
   showCategory: boolean = false;
 
   categoryList: any[] = [
-    {name:'Fonts'},
-    {name:'Stock Photos'},
-    {name:'Stock Videos'},
-    {name:'SVG Images'},
-    {name:'Icons'},
-    {name:'Illustrations'},
-    {name:'Patterns'}
+    { name: 'Fonts' },
+    { name: 'Stock Photos' },
+    { name: 'Stock Videos' },
+    { name: 'SVG Images' },
+    { name: 'Icons' },
+    { name: 'Illustrations' },
+    { name: 'Patterns' }
   ]
 
   showHideCategoryPc() {
-    console.log("Clicked", this.categoryList)
+    // console.log("Clicked", this.categoryList)
     this.showCategory = !this.showCategory;
   }
 
   HideCategoryPc() {
-    console.log("Clicked Ouside")
+    // console.log("Clicked Ouside")
     this.showCategory = false;
+    if ($('#nav_link_container').hasClass("active")) {
+      $('#nav_link_container').toggleClass("active");
+      $('#nav_link_container_i').toggleClass("fa-xmark")
+    }
+  }
+
+  navButtonClick() {
+    $('#nav_link_container').toggleClass("active");
+    $('#nav_link_container_i').toggleClass("fa-xmark")
+
   }
 }
